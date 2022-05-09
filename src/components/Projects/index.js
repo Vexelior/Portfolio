99605faab857a6d429021e5032f016da8b37004f
Alexander Sanderson <67873187+Vexelior@users.noreply.github.com>
2022-05-08 21:04:23 -0700

    added bootstrap

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
-import { Card, Button } from 'react-bootstrap'
+import { Card, Button } from 'react-bootstrap/'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './index.scss'
 
@@ -13,6 +13,48 @@ const Projects = () => {
       setLetterClass('text-animate-hover')
     }, 3000)
   }, [])
+
+  // Create projects array
+  const projects = [
+    {
+      title: 'Project 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod, nunc euismod euismod consectetur, nisi nunc lobortis nisl, euismod euismod nunc nisi euismod.',
+      image: 'https://via.placeholder.com/300x200',
+      link: '#'
+    },
+    {
+      title: 'Project 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod, nunc euismod euismod consectetur, nisi nunc lobortis nisl, euismod euismod nunc nisi euismod.',
+      image: 'https://via.placeholder.com/300x200',
+      link: '#'
+    },
+    {
+      title: 'Project 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod, nunc euismod euismod consectetur, nisi nunc lobortis nisl, euismod euismod nunc nisi euismod.',
+      image: 'https://via.placeholder.com/300x200',
+      link: '#'
+    },
+    {
+      title: 'Project 4',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod, nunc euismod euismod consectetur, nisi nunc lobortis nisl, euismod euismod nunc nisi euismod.',
+      image: 'https://via.placeholder.com/300x200',
+      link: '#'
+    },
+    {
+      title: 'Project 5',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod, nunc euismod euismod consectetur, nisi nunc lobortis nisl, euismod euismod nunc nisi euismod.',
+      image: 'https://via.placeholder.com/300x200',
+      link: '#'
+    },
+    {
+      title: 'Project 6',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod, nunc euismod euismod consectetur, nisi nunc lobortis nisl, euismod euismod nunc nisi euismod.',
+      image: 'https://via.placeholder.com/300x200',
+      link: '#'
+    }
+  ];
+
+
 
   return (
     <>
@@ -34,76 +76,23 @@ const Projects = () => {
         </div>
         <div className="projects-container">
             <div className="projects-container-inner">
-                <Card>
-                    <Card.Header>Featured</Card.Header>
-                    <Card.Body>
-                        <Card.Title>Special title treatment</Card.Title>
-                        <Card.Text>
-                        With supporting text below as a natural lead-in to additional content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <br />
-                <br />
-                <Card>
-                    <Card.Header>Featured</Card.Header>
-                    <Card.Body>
-                        <Card.Title>Special title treatment</Card.Title>
-                        <Card.Text>
-                        With supporting text below as a natural lead-in to additional content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <br />
-                <br />
-                <Card>
-                    <Card.Header>Featured</Card.Header>
-                    <Card.Body>
-                        <Card.Title>Special title treatment</Card.Title>
-                        <Card.Text>
-                        With supporting text below as a natural lead-in to additional content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <br />
-                <br />
-                <Card>
-                    <Card.Header>Featured</Card.Header>
-                    <Card.Body>
-                        <Card.Title>Special title treatment</Card.Title>
-                        <Card.Text>
-                        With supporting text below as a natural lead-in to additional content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <br />
-                <br />
-                <Card>
-                    <Card.Header>Featured</Card.Header>
-                    <Card.Body>
-                        <Card.Title>Special title treatment</Card.Title>
-                        <Card.Text>
-                        With supporting text below as a natural lead-in to additional content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <br />
-                <br />
-                <Card>
-                    <Card.Header>Featured</Card.Header>
-                    <Card.Body>
-                        <Card.Title>Special title treatment</Card.Title>
-                        <Card.Text>
-                        With supporting text below as a natural lead-in to additional content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
+                {projects.map((project, idx) => {
+                    return (
+                      
+                        <Card key={idx} className="project-card">
+                            <Card.Img variant="top" src={project.image} />
+                            <Card.Body>
+                                <Card.Title>{project.title}</Card.Title>
+                                <Card.Text>
+                                    {project.description}
+                                </Card.Text>
+                                <Button variant="primary" href={project.link}>
+                                    <FontAwesomeIcon icon="arrow-right" />
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    )
+                })}
             </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
